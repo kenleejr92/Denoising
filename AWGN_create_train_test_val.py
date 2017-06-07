fdbf169baec05bd52232875cfee5f791300ed62c
kenleejr92 <kennethlee@utexas.edu>
2017-06-06 19:32:34 -0500

    denoising works

--- AWGN_create_train_test_val.py
+++ AWGN_create_train_test_val.py
@@ -6,11 +6,16 @@ import glob
 
 NOISE_LEVELS = [15, 25, 35, 45, 55]
 
-display = 150
+display = 5
 data_dir = '/mnt/hdd1/BSR/BSDS500/data/images'
 
-def crop(image,offset):
-    return image[offset:offset+180,offset:offset+180,:]
+def crop(image, size=50):
+    width = image.shape[0]
+    height = image.shape[1]
+
+    h = np.random.randint(low=0, high=height-50)
+    w = np.random.randint(low=0, high=width-50)
+    return image[w:w+50, h:h+50, :]
 
 def make_dirs():
     if not os.path.exists(data_dir + '/AWGN/train'):
@@ -20,12 +25,20 @@ def make_dirs():
     if not os.path.exists(data_dir + '/AWGN/val'):
         os.makedirs(data_dir + '/AWGN/val')
 
-def save_images(dir,x,y):
+def save_images(dir, x, y):
     i=0
     for _x, _y in zip(x.astype('uint8'), y.astype('uint8')):
         io.imsave(data_dir+'/AWGN/' + dir + '/' + str(i) + '_x' + '.jpg', _x)
         io.imsave(data_dir+'/AWGN/' + dir + '/' + str(i) + '_y' + '.jpg', _y)
         i = i + 1
+
+def sample_from_image(image, num_samples=20, size=50):
+    samples = np.empty((1, size, size, 3), np.uint8)
+    for i in range(num_samples):
+        cropped = crop(image, size)
+        cropped = np.expand_dims(cropped, axis=0)
+        samples = np.vstack((samples, cropped))
+    return samples[1:, :, :, :]
 
 def img_collection_to_numpy(img_collection):
     tensor = [img_collection[i] for i in range(len(img_collection))]
@@ -44,23 +57,30 @@ def create_AWGN_train_test_val(residual_learning=True):
     val = io.imread_collection(data_dir + '/val/*.jpg')
     test = io.imread_collection(data_dir + '/test/*.jpg')
 
-    train_tensor = [train[i] for i in range(len(train))]
-    test_tensor = [test[i] for i in range(len(test))]
-    val_tensor = [val[i] for i in range(len(val))]
+    train_arr = [train[i] for i in range(len(train))]
+    test_arr = [test[i] for i in range(len(test))]
+    val_arr = [val[i] for i in range(len(val))]
 
-    for i, t in enumerate(train_tensor):
-        train_tensor[i] = crop(t,100)
-    for i, t in enumerate(test_tensor):
-        test_tensor[i] = crop(t,100)
-    for i, t in enumerate(val_tensor):
-        val_tensor[i] = crop(t,100)
+    train_tensor = np.zeros((20*len(train_arr), 50, 50, 3))
+    test_tensor = np.zeros((20*len(test_arr), 50, 50, 3))
+    val_tensor = np.zeros((20*len(val_arr), 50, 50, 3))
+
+    for i, t in enumerate(train_arr):
+        train_tensor[i*20:i*20+20, :, :, :] = sample_from_image(t)
+        # if (i*20)%100 == 0:
+        #     io.imshow(train_tensor[i*20])
+        #     io.show()
+    for i, t in enumerate(test_arr):
+        test_tensor[i*20:i*20+20, :, :, :] = sample_from_image(t)
+    for i, t in enumerate(val_arr):
+        val_tensor[i*20:i*20+20, :, :, :] = sample_from_image(t)
 
     train_tensor = np.array(train_tensor)
     test_tensor = np.array(test_tensor)
     val_tensor = np.array(val_tensor)
     clean_tensor = np.vstack((train_tensor, test_tensor, val_tensor)).astype(np.int16)
     np.random.shuffle(clean_tensor)
-    noise_shape = [100] + list(clean_tensor.shape[1:])
+    noise_shape = [2000] + list(clean_tensor.shape[1:])
     noise0 = np.random.normal(loc=0.0, scale=NOISE_LEVELS[0], size=noise_shape)
     noise1 = np.random.normal(loc=0.0, scale=NOISE_LEVELS[1], size=noise_shape)
     noise2 = np.random.normal(loc=0.0, scale=NOISE_LEVELS[2], size=noise_shape)
@@ -76,24 +96,24 @@ def create_AWGN_train_test_val(residual_learning=True):
 
 
     if residual_learning == True:
-        train_x = noisy_tensor[0:300, :, :, :]
-        train_y = noise[0:300, :, :, :]
+        train_x = noisy_tensor[0:8000, :, :, :]
+        train_y = noise[0:8000, :, :, :]
 
-        val_x = noisy_tensor[300:400, :, :, :]
-        val_y = noise[300:400, :, :, :]
+        val_x = noisy_tensor[8000:9000, :, :, :]
+        val_y = noise[8000:9000, :, :, :]
 
-        test_x = noisy_tensor[400:, :, :, :]
-        test_y = noise[400:, :, :, :]
+        test_x = noisy_tensor[9000:, :, :, :]
+        test_y = noise[9000:, :, :, :]
 
     else:
-        train_x = noisy_tensor[0:300, :, :, :]
-        train_y = clean_tensor[0:300, :, :, :]
+        train_x = noisy_tensor[0:8000, :, :, :]
+        train_y = clean_tensor[0:8000, :, :, :]
 
-        val_x = noisy_tensor[300:400, :, :, :]
-        val_y = clean_tensor[300:400, :, :, :]
+        val_x = noisy_tensor[8000:9000, :, :, :]
+        val_y = clean_tensor[8000:9000, :, :, :]
 
-        test_x = noisy_tensor[400:, :, :, :]
-        test_y = clean_tensor[400:, :, :, :]
+        test_x = noisy_tensor[9000:, :, :, :]
+        test_y = clean_tensor[9000:, :, :, :]
 
     save_images('train', train_x, train_y)
     save_images('test', test_x, test_y)
--- DnCNN.py
+++ DnCNN.py
@@ -10,33 +10,21 @@ def weight_variable(shape, Name):
     initial = tf.truncated_normal(shape, stddev=0.1)
     return tf.Variable(initial, name=Name)
 
+
 def bias_variable(shape, Name):
     initial = tf.constant(0.1, shape=shape)
     return tf.Variable(initial, name=Name)
+
 
 def offset_variable(shape, Name):
     initial = tf.constant(0.0, shape=shape)
     return tf.Variable(initial, name=Name)
 
+
 def scale_variable(shape, Name):
     initial = tf.constant(1.0, shape=shape)
     return tf.Variable(initial, name=Name)
 
-def conv2d(x, W, b, layer_name, act=tf.nn.relu,):
-    with tf.name_scope(layer_name):
-        with tf.name_scope('weights'):
-            variable_summaries(W)
-        with tf.name_scope('biases'):
-            variable_summaries(b)
-        with tf.name_scope('W_plus_b'):
-            preactivate = tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME') + b
-            tf.summary.histogram('pre_activations', preactivate)
-        if act == None:
-            activations = tf.identity(preactivate, name='activation')
-        else: 
-            activations = act(preactivate, name='activation')
-        tf.summary.histogram('activations', activations)
-    return activations
 
 def variable_summaries(var):
     with tf.name_scope('summaries'):
@@ -48,6 +36,37 @@ def variable_summaries(var):
         tf.summary.scalar('max', tf.reduce_max(var))
         tf.summary.scalar('min', tf.reduce_min(var))
         tf.summary.histogram('histogram', var)
+
+
+def conv2d_with_BN(x, kernel_size, input_channels, output_channels, layer_name, BN=True, act=True):
+    W = weight_variable([kernel_size, kernel_size, input_channels, output_channels], 'W_' + layer_name)
+    b = bias_variable([output_channels], 'b_' + layer_name)
+    o = offset_variable([x.get_shape().as_list()[1], x.get_shape().as_list()[1], output_channels], 'o_' + layer_name)
+    s = scale_variable([x.get_shape().as_list()[1], x.get_shape().as_list()[1], output_channels], 's_' + layer_name)
+
+    with tf.name_scope(layer_name):
+        with tf.name_scope('weights'):
+            variable_summaries(W)
+        with tf.name_scope('biases'):
+            variable_summaries(b)
+        with tf.name_scope('Wx_plus_b'):
+            preactivate = tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME') + b
+            tf.summary.histogram('pre_activations', preactivate)
+        if BN == True and act==True:
+            with tf.name_scope('BN_Wx_plus_b'):
+                batch_mean1, batch_var1 = tf.nn.moments(preactivate,[0])
+                BN_preactivate = tf.nn.batch_normalization(preactivate, batch_mean1, batch_var1, o, s, 0.01)
+                tf.summary.histogram('BN_pre_activations', BN_preactivate)
+                activations = tf.nn.relu(BN_preactivate, name='activation')
+        elif BN == False and act == True:
+            activations = tf.nn.relu(preactivate, name='activation')
+        elif BN == False and act == False:
+            activations = tf.identity(preactivate, name='activation')   
+        tf.summary.histogram('activations', activations)
+
+    return activations
+
+
 
 class denoising_autoencoder(object):
     def __init__(self):
@@ -65,29 +84,6 @@ class denoising_autoencoder(object):
 
         self.img_dim = list(self.train_x.shape[1:])
 
-        #place_holders
-        self.x_ = None
-        self.y_ = None
-
-        #tf variables
-        self.W1 = None
-        self.W2 = None
-        self.W3 = None
-        self.b1 = None
-        self.b2 = None
-        self.b3 = None
-
-
-        #tf ops on the variables
-        self.MSE = None
-        self.PSNR = None
-        self.h_conv1 = None
-        self.h_conv2 = None
-        self.output = None
-
-        #tensorflow objects
-        self.tf_session = None
-        self.tf_saver = None
         self.save_path = '/home/kenleejr92/Denoising/tmp/DnCNN'
 
 
@@ -100,104 +96,111 @@ class denoising_autoencoder(object):
 
         return x_, y_
 
-    def create_variables(self):
-        W1 = weight_variable([3, 3, 3, 32], 'W1')
-        b1 = bias_variable([32], 'b1')
 
-        o1 = offset_variable([3, 3, 3, 32], 'o1')
-        s1 = scale_variable([3, 3, 3, 32], 's1')
-
-        W2 = weight_variable([3, 3, 32, 64], 'W2')
-        b2 = bias_variable([64], 'b2')
-
-        o2 = offset_variable([3, 3, 32, 64], 'o1')
-        s2 = scale_variable([3, 3, 32, 64], 's1')
-
-        W3 = weight_variable([3, 3, 64, 3], 'W3')
-        b3 = bias_variable([3], 'b3')
-
-        o1 = offset_variable([3, 3, 3, 32], 'o1')
-        s1 = scale_variable([3, 3, 3, 32], 's1')
-
-        return W1, b1, W2, b2, W3, b3
-
-
-    def create_model(self, x_batch, y_batch):
+    def create_model(self, x_batch, y_batch, num_layers=19):
         """ 
         Build the denoiser
         """
         input_layer = tf.reshape(x_batch,[-1] + self.img_dim)
-        self.W1, self.b1, self.W2, self.b2, self.W3, self.b3 = self.create_variables()
 
-        self.h_conv1 = conv2d(input_layer, self.W1, self.b1, 'conv1')
-        self.h_conv2 = conv2d(self.h_conv1, self.W2, self.b2, 'conv2')
-        self.output = conv2d(self.h_conv2, self.W3, self.b3, 'conv3', act=None)
+        h_conv0 = conv2d_with_BN(input_layer, kernel_size=3, input_channels=3, output_channels=32, layer_name='conv0', BN=False, act=True)
+        tf.add_to_collection('h_conv0', h_conv0)
+        layer_array = [h_conv0]
+        for i in np.arange(1, num_layers-2):
+            h_conv = conv2d_with_BN(layer_array[i-1], 3, layer_array[i-1].get_shape().as_list()[3], 64, 'h_conv' + str(i))
+            tf.add_to_collection('h_conv' + str(i), h_conv)
+            layer_array.append(h_conv)
 
-        self.MSE = tf.reduce_mean(tf.square(self.output - y_batch))
-        with tf.name_scope('MSE'):
-            tf.summary.scalar('MSE',self.MSE)
-        self.PSNR = (255**2)/self.MSE
+        output = conv2d_with_BN(layer_array[-1], 3, layer_array[i-1].get_shape().as_list()[3], 3, 'h_conv' + str(num_layers), BN=False, act=False)
+        MSE = tf.reduce_mean(tf.square(output - y_batch))
+        with tf.name_scope('metrics'):
+            tf.summary.scalar('MSE', MSE)
+            variable_summaries(output)
 
-        tf.add_to_collection('h_conv1', self.h_conv1)
-        tf.add_to_collection('h_conv2', self.h_conv2)
-        tf.add_to_collection('output', self.output)
-        tf.add_to_collection('MSE', self.MSE)
+        PSNR = (255**2)/MSE
+
+        tf.add_to_collection('output', output)
+        tf.add_to_collection('MSE', MSE)
+
+        return output, MSE, PSNR
 
 
-    def restore_model(self, global_step=5):
-        self.W1, self.b1, self.W2, self.b2, self.W3, self.b3 = self.create_variables()
+    def restore_model(self, tf_session, global_step=50):
         new_saver = tf.train.import_meta_graph(self.save_path + '-' + str(global_step) + '.meta')
-        new_saver.restore(self.tf_session, tf.train.latest_checkpoint('/home/kenleejr92/Denoising/tmp/'))
+        new_saver.restore(tf_session, tf.train.latest_checkpoint('/home/kenleejr92/Denoising/tmp/'))
 
-        self.h_conv1 = tf.get_collection('h_conv1')
-        self.h_conv2 = tf.get_collection('h_conv2')
         self.output = tf.get_collection('output')
         self.MSE = tf.get_collection('MSE')
 
 
-    def train(self, epochs=100, batch_size=10):
+    def train(self, epochs=50, batch_size=128):
         """
         Training
         """
-        self.x_, self.y_ = self.create_placeholders()
-        self.create_model(self.x_, self.y_)
-        self.tf_saver = tf.train.Saver([self.W1, self.W2, self.W3, self.b1, self.b2, self.b3])
-        self.tf_session = tf.Session()
+        x_, y_ = self.create_placeholders()
+        output, MSE, PSNR = self.create_model(x_, y_)
+        tf_saver = tf.train.Saver()
+        tf_session = tf.Session()
         merged = tf.summary.merge_all()
-        train_writer = tf.summary.FileWriter('/home/kenleejr92/Denoising/tmp/train', self.tf_session.graph)
-        train_step = tf.train.AdamOptimizer(1e-4).minimize(self.MSE)
+        train_writer = tf.summary.FileWriter('/home/kenleejr92/Denoising/tmp/train', tf_session.graph)
+        val_writer = tf.summary.FileWriter('/home/kenleejr92/Denoising/tmp/val', tf_session.graph)
+        train_step = tf.train.AdamOptimizer(1e-4).minimize(MSE)
         init_op = tf.global_variables_initializer()
-        self.tf_session.run(init_op)
+        tf_session.run(init_op)
 
-        for epoch in range(epochs):
-            if epoch%10 == 0:
+        for epoch in range(epochs+1):
+            if epoch%5 == 0:
                 print('Step %d' % epoch)
-                tr_feed = {self.y_: self.train_y[epoch:epoch+batch_size], self.x_: self.train_x[epoch:epoch+batch_size]}
-                print 10*np.log10(self.tf_session.run(self.PSNR, feed_dict = tr_feed))
-                self.tf_saver.save(self.tf_session, self.save_path, global_step=epoch)
-                summary = self.tf_session.run(merged, feed_dict=tr_feed)
+                tr_feed = {y_: self.train_y[epoch:epoch+batch_size], x_: self.train_x[epoch:epoch+batch_size]}
+                print tf_session.run(MSE, feed_dict = tr_feed)
+                tf_saver.save(tf_session, self.save_path, global_step=epoch)
+                summary = tf_session.run(merged, feed_dict=tr_feed)
                 train_writer.add_summary(summary, epoch)
 
             for i in np.arange(0,self.train_x.shape[0],batch_size):
-                tr_feed = {self.y_: self.train_y[epoch:epoch+batch_size], self.x_: self.train_x[epoch:epoch+batch_size]}
-                self.tf_session.run(train_step, feed_dict=tr_feed)
+                tr_feed = {y_: self.train_y[epoch:epoch+batch_size], x_: self.train_x[epoch:epoch+batch_size]}
+                tf_session.run(train_step, feed_dict=tr_feed)
 
 
     def inference(self):
-        self.tf_session = tf.Session()
-        self.restore_model()
-        inf_feed = {'noisy_image:0': self.test_x[0:5]}
-        residual_noise =  self.tf_session.run(self.output, feed_dict=inf_feed)
-        clean_images = np.squeeze(self.test_x[0:5] - residual_noise)
+        tf_session = tf.Session()
+        self.restore_model(tf_session)
+        inf_feed = {'noisy_image:0': self.test_x[10:20]}
+        residual_noise =  tf_session.run(self.output, feed_dict=inf_feed)
+        clean_images = np.clip(np.squeeze(self.test_x[10:20] - residual_noise), 0, 255).astype(np.uint8)
+        original_img = np.clip(np.squeeze(self.test_x[10:20] - self.test_y[10:20]), 0, 255).astype(np.uint8)
+        for i in range(10):
+            io.imshow_collection([self.test_x[i+10], clean_images[i], original_img[i]])
+            io.show()
 
-        io.imshow(clean_images[0])
-        io.show()
-
-
+    def denoise_img(self, image):
+        tf_session = tf.Session()
+        self.restore_model(tf_session)
+        width = image.shape[0]
+        height = image.shape[1]
+        patches = np.zeros((width, height, 3))
+        for h in np.arange(0, height-50, 50):
+            for w in np.arange(0, width-50, 50):
+                patch = np.expand_dims(image[h:h+50, w:w+50, :], axis=0)
+                if patch.shape != (1, 50, 50, 3): continue
+                inf_dict = {'noisy_image:0': patch}
+                patches[h:h+50, w:w+50, :] = patch.astype(np.int16) - np.squeeze(tf_session.run(self.output, feed_dict=inf_dict))
+            
+        return patches
+        
 
 
 if __name__ == '__main__':
     dae = denoising_autoencoder()
-    dae.train()
-    #dae.inference()
+    # dae.train()
+    # dae.inference()
+    clean = io.imread('/mnt/hdd1/BSR/BSDS500/data/images/test/226022.jpg')
+    noise = np.rint(np.random.normal(loc=0.0, scale=55.0, size=clean.shape))
+    noisy = np.clip(clean.astype(np.int16) + noise, 0, 255).astype(np.uint8)
+    denoised = dae.denoise_img(noisy)
+    noisy = np.clip(noisy, 0, 255)
+    denoised = np.rint(np.clip(denoised, 0, 255)).astype(np.uint8)
+    io.imshow_collection([clean, noisy, denoised])
+    io.show()
+
     
